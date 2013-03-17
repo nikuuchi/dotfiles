53d1b240fe8f54567807b7bf56930a597873f548
uchida <uchida.atsushi.8@gmail.com>
2013-03-17 21:45:28 +0900

    add tmux.conf

--- setup.sh
+++ setup.sh
@@ -3,9 +3,17 @@
 git submodule init
 git submodule update
 
-DOTFILES=(.zsh .zshrc .vim .vimrc)
+DOTFILES=(.zsh .zshrc .vim .vimrc .tmux.conf)
 
 for var in ${DOTFILES[@]}
 do
 	ln -s `pwd`/$var $HOME/$var
 done
+
+ex <<EOS
+NeoBundleInstall
+EOS
+
+if [ -d $HOME/.vim/bundle/vimproc ]; then
+	make -C $HOME/.vim/bundle/vimproc -fmake_unix.mak
+fi
